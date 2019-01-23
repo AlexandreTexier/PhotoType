0ad82b06252ba1ff4c2757bd13caef9b9c8da080
AlexandreTexier <a.texier@outlook.com>
2019-01-23 18:33:00 +0100

    variable + code on specimen

--- site/script.js
+++ site/script.js
@@ -1,7 +1,27 @@
-// ===== GLOBAL VARIABLES ======
+// ===== GLOBAL VARIABLES ====== //
+var variableWeight;
+var weight
 
 
-// ===== DOM READY ======
 
+// ===== DOM READY ====== //
+$(document).ready(function() {
 
-// ===== FUNCTIONS ======
+  console.log("ready");
+
+$("#variable-font-slider").on("input change", function() {
+  variableWeight = $("#variable-font-slider").val();
+  $("#variable-font-slider").attr("value", variableWeight);
+  weight= $("#variable-font-slider").attr("value");
+  console.log("changing value : "+variableWeight+" et :"+weight);
+  $(":root").css("--variable-weight", variableWeight.toString() );
+});
+  // Getter
+// variableWeight = $( ".selector" ).slider( "option", "value" );
+// console.log(variableWeight);
+
+// Setter
+// $( "#variable-font-slider" ).slider( "option", "value", 10 );
+
+});
+// ===== FUNCTIONS ====== //
